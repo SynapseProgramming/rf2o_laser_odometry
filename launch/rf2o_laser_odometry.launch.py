17dfc3404ad8855db8599c4c53805b7d9d3fadcd
Roald Ong <roald.ong@gmail.com>
2023-07-31 13:48:30 +0800

    modified lidar odometry topic

--- launch/rf2o_laser_odometry.launch.py
+++ launch/rf2o_laser_odometry.launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
                 output='screen',
                 parameters=[{
                     'laser_scan_topic' : '/scan',
-                    'odom_topic' : '/odom_rf2o',
+                    'odom_topic' : '/lidar_odom',
                     'publish_tf' : False,
                     'base_frame_id' : 'base_footprint',
                     'odom_frame_id' : 'odom',
